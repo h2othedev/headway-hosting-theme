4455004f36e6627fbcf7a2cf53e434abe83d64b5
Abdelhady Salah <hadysalah1455@gmail.com>
2022-12-14 15:26:12 +0200

    Implement numbers counter plus optimize code

--- script.js
+++ script.js
@@ -16,43 +16,21 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-window.onload = () => {
-    //Navigation
-    const headerNav = document.querySelector('.header-nav')
-    headerNav.addEventListener('mouseover', (e) => {
-        if (e.target.tagName === 'LI' && !e.target.classList.contains('active')) {
-            e.target.classList.add('secondary-light-blue-border-bottom')
-        }
+const mobileNav = () => {
+    /** Mobile Nav **/
+    const mobNavElement = document.querySelector('.mobile-menu-nav')
+    mobNavElement.addEventListener('click', () => {
+        const headerNav = document.querySelector('.header-nav')
+        headerNav.classList.toggle('header-nav-show')
     })
-    headerNav.addEventListener('mouseout', (e) => {
-        if (e.target.tagName === 'LI' && !e.target.classList.contains('active')) {
-            e.target.classList.remove('secondary-light-blue-border-bottom')
-        }
+    const closeOverlayElement = document.querySelector('.icon-mobile-close-overlay')
+    closeOverlayElement.addEventListener('click', () => {
+        const headerNav = document.querySelector('.header-nav')
+        headerNav.classList.toggle('header-nav-show')
     })
-    //Testimonials
-    let activeTestimonial = 0
-    const testimonials = document.querySelectorAll('.testimonial')
-    const maxTestimonials = testimonials.length
-    const changeTestimonial = (n) => {
-        if (n >= maxTestimonials) {
-            n = 0
-        }
-        if (n < 0) {
-            n = maxTestimonials - 1
-        }
-        activeTestimonial = n
-        for (const testimonial of testimonials) {
-            if (testimonial.classList.contains('testimonial-active')) {
-                testimonial.classList.remove('testimonial-active')
-            }
-        }
-        testimonials[activeTestimonial].classList.add('testimonial-active')
-    }
-    const testimonialPrev = document.querySelector('.testimonial-prev')
-    const testimonialNext = document.querySelector('.testimonial-next')
-    testimonialPrev && testimonialPrev.addEventListener('click', () => changeTestimonial(activeTestimonial - 1))
-    testimonialNext && testimonialNext.addEventListener('click', () => changeTestimonial(activeTestimonial + 1))
-    //Slideshow
+}
+
+const slideShow = () => {
     let activeSlider = 0
     const sliders = document.querySelectorAll('.slider')
     const maxSliders = sliders.length
@@ -75,15 +53,94 @@ window.onload = () => {
     const sliderNext = document.querySelector('.slider-next')
     sliderPrev && sliderPrev.addEventListener('click', () => changeSlider(activeSlider - 1))
     sliderNext && sliderNext.addEventListener('click', () => changeSlider(activeSlider + 1))
-    /** Mobile Nav **/
-    const mobNavElement = document.querySelector('.mobile-menu-nav')
-    mobNavElement.addEventListener('click', () => {
-        const headerNav = document.querySelector('.header-nav')
-        headerNav.classList.toggle('header-nav-show')
+}
+
+const testimonials = () => {
+    let activeTestimonial = 0
+    const testimonials = document.querySelectorAll('.testimonial')
+    const maxTestimonials = testimonials.length
+    const changeTestimonial = (n) => {
+        if (n >= maxTestimonials) {
+            n = 0
+        }
+        if (n < 0) {
+            n = maxTestimonials - 1
+        }
+        activeTestimonial = n
+        for (const testimonial of testimonials) {
+            if (testimonial.classList.contains('testimonial-active')) {
+                testimonial.classList.remove('testimonial-active')
+            }
+        }
+        testimonials[activeTestimonial].classList.add('testimonial-active')
+    }
+    const testimonialPrev = document.querySelector('.testimonial-prev')
+    const testimonialNext = document.querySelector('.testimonial-next')
+    testimonialPrev && testimonialPrev.addEventListener('click', () => changeTestimonial(activeTestimonial - 1))
+    testimonialNext && testimonialNext.addEventListener('click', () => changeTestimonial(activeTestimonial + 1))
+}
+
+const headerNav = () => {
+    const headerNav = document.querySelector('.header-nav')
+    headerNav.addEventListener('mouseover', (e) => {
+        if (e.target.tagName === 'LI' && !e.target.classList.contains('active')) {
+            e.target.classList.add('secondary-light-blue-border-bottom')
+        }
     })
-    const closeOverlayElement = document.querySelector('.icon-mobile-close-overlay')
-    closeOverlayElement.addEventListener('click', () => {
-        const headerNav = document.querySelector('.header-nav')
-        headerNav.classList.toggle('header-nav-show')
+    headerNav.addEventListener('mouseout', (e) => {
+        if (e.target.tagName === 'LI' && !e.target.classList.contains('active')) {
+            e.target.classList.remove('secondary-light-blue-border-bottom')
+        }
     })
+}
+
+window.onload = () => {
+    headerNav()
+    testimonials()
+    slideShow()
+    mobileNav()
+}
+
+/** Stats Feature */
+
+let statsActivated = false
+
+const countStat = (increment, cur, end) => {
+    if ((cur + increment) > end) {
+        return (cur = end)
+    }
+    cur = (cur + increment)
+    return cur
+}
+
+const reflectStatUpdate = (element, increment, cur, end) => {
+    const countCurStat = countStat(increment, cur, end)
+    element.textContent = cur
+    countCurStat !== end && setTimeout(() => reflectStatUpdate(element, 1000, countCurStat, end), 50)
+    countCurStat === end && (element.textContent += '+')
+}
+
+const countStats = () => {
+    if (statsActivated === true) return false
+    statsActivated = true
+    const statsNumbers = document.querySelectorAll('.stats-number')
+    for (let statsNumber of statsNumbers) {
+        setTimeout(() => reflectStatUpdate(statsNumber, 1000, 0, statsNumber.textContent), 1000)
+    }
+}
+
+const checkStats = () => {
+    /** Stats Numbers **/
+    const statsContainerElement = document.querySelector('.stats-container')
+    if (statsContainerElement) {
+        const rect = statsContainerElement.getBoundingClientRect()
+        const offsetY = rect.y
+        if (offsetY <= 200 && offsetY >= -200) {
+            countStats()
+        }
+    }
+}
+
+window.onscroll = () => {
+    !statsActivated && checkStats()
 }
